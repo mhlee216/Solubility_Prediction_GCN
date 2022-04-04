619a8d6991015d5f37c383e278dc8cec28b898bf
mhlee <leemh216@gmail.com>
2022-04-04 10:13:57 +0900

    Update class_run.sh

--- class_run.sh
+++ class_run.sh
@@ -1,3 +1,1 @@
-python classification.py -D './data/data01.xlsx' -X1 'Solute SMILES' -X2 'Solvent SMILES' -Y 'LogS' -O './results/classification/data01C.json' -M './results/classification/myModel1C.pt' -ROC
-# python classification.py -D './data/data02.xlsx' -X1 'Solute SMILES' -X2 'Solvent SMILES' -Y 'LogS' -O './results/classification/data02C.json' -M './results/classification/myModel2C.pt' -ROC
-# python classification.py -D './data/data03.xlsx' -X1 'Solute SMILES' -X2 'Solvent SMILES' -Y 'LogS' -O './results/classification/data03C.json' -M './results/classification/myModel3C.pt' -ROC
+python classification.py -D './data/data01.csv' -X1 'Solute SMILES' -X2 'Solvent SMILES' -Y 'LogS' -O './results/classification/data01C.json' -M './results/classification/myModel1C.pt' -ROC
