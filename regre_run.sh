cb28f115f6c345daf67a7f7f3051831cd995f5d8
mhlee <leemh216@gmail.com>
2022-04-04 10:13:44 +0900

    Update regre_run.sh

--- regre_run.sh
+++ regre_run.sh
@@ -1,3 +1,1 @@
-python regression.py -D './data/data01.xlsx' -X1 'Solute SMILES' -X2 'Solvent SMILES' -Y 'LogS' -O './results/regression/data01R.json' -M './results/regression/myModel1R.pt'
-# python regression.py -D './data/data02.xlsx' -X1 'Solute SMILES' -X2 'Solvent SMILES' -Y 'LogS' -O './results/regression/data02R.json' -M './results/regression/myModel2R.pt'
-# python regression.py -D './data/data03.xlsx' -X1 'Solute SMILES' -X2 'Solvent SMILES' -Y 'LogS' -O './results/regression/data03R.json' -M './results/regression/myModel3R.pt'
+python regression.py -D './data/data01.csv' -X1 'Solute SMILES' -X2 'Solvent SMILES' -Y 'LogS' -O './results/regression/data01R.json' -M './results/regression/myModel1R.pt'
